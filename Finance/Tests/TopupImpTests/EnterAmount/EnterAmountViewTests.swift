2112e600c672da39b1d28c09b10d7b33ec4d5a99
randychoi95 <jhc130833@naver.com>
2021-11-29 13:53:26 +0900

    - Snapshot Testing

---
+++ Finance/Tests/TopupImpTests/EnterAmount/EnterAmountViewTests.swift
@@ -0,0 +1,79 @@
+//
+//  File.swift
+//  
+//
+//  Created by 최제환 on 2021/11/29.
+//
+
+import Foundation
+@testable import TopupImp
+import XCTest
+import SnapshotTesting
+import FinanceEntity
+
+final class EnterAmountViewTests: XCTestCase {
+    
+    func testEnterAmount() {
+        // given
+        let paymentMethod = PaymentMethod(
+            id: "0",
+            name: "슈퍼은행",
+            digits: "**** 9999",
+            color: "#51AF80FF",
+            isPrimary: false
+        )
+        let viewModel = SelectedPaymentMethodViewModel(paymentMethod)
+        
+        // when
+        
+        let sut = EnterAmountViewController()
+        sut.updateSelectedPaymentMethod(with: viewModel)
+        
+        // then
+        assertSnapshot(matching: sut, as: .image(on: .iPhoneXsMax))
+    }
+    
+    func testEnterAmountLoading() {
+        // given
+        let paymentMethod = PaymentMethod(
+            id: "0",
+            name: "슈퍼은행",
+            digits: "**** 9999",
+            color: "#51AF80FF",
+            isPrimary: false
+        )
+        let viewModel = SelectedPaymentMethodViewModel(paymentMethod)
+        
+        // when
+        
+        let sut = EnterAmountViewController()
+        sut.updateSelectedPaymentMethod(with: viewModel)
+        sut.startLoading()
+        
+        // then
+        assertSnapshot(matching: sut, as: .image(on: .iPhoneXsMax))
+    }
+    
+    func testEnterAmountStopLoading() {
+        // given
+        let paymentMethod = PaymentMethod(
+            id: "0",
+            name: "슈퍼은행",
+            digits: "**** 9999",
+            color: "#51AF80FF",
+            isPrimary: false
+        )
+        let viewModel = SelectedPaymentMethodViewModel(paymentMethod)
+        
+        // when
+        
+        let sut = EnterAmountViewController()
+        sut.updateSelectedPaymentMethod(with: viewModel)
+        sut.startLoading()
+        sut.stopLoading()
+        
+        // then
+        assertSnapshot(matching: sut, as: .image(on: .iPhoneXsMax))
+    }
+    
+}
